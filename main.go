74ea9a0ee2db6014c64acfe1925dbfb01e6d219f
Arthur Aslanyan <arthur.e.aslanyan@gmail.com>
2021-10-04 12:30:48 +0600

    Flag for tomorrow
    - [ADDED] Tomorrow flag
    - [ADDED] Updated TODO

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"os"
@@ -12,8 +13,20 @@ import (
 // TODO: Test no just happy path - have to tell all those vars are necessary
 // TODO If Tg is not set up correctly - there is no error
 func main() {
+	tomorrow := flag.Bool("tomorrow", false, "Use this flag if you need tomorrow events")
+	flag.Parse()
+
 	cal := pkg.NewGCalStore(os.Getenv("GCALID"), option.WithCredentialsFile(os.Getenv("GCALCRED")))
-	es, err := pkg.TomorrowEvents(cal)
+
+	var (
+		es  []pkg.Event
+		err error
+	)
+	if *tomorrow {
+		es, err = pkg.TomorrowEvents(cal)
+	} else {
+		es, err = pkg.TodayEvents(cal)
+	}
 	if err != nil {
 		log.Fatal(err)
 	}
